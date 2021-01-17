96e801050a9de55d1b48f60d740926a55f0e1ecf
Edison Peñuela <edisonpe961206@hotmail.com>
2021-01-16 21:47:11 -0500

    Fix Service Worker Registration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { initView } from '@View';
 import { addListeners, runDraw } from '@Controller';
 import { initWorkers } from '@Model';
+import { registerSW } from './serviceWorkerRegistration';
 
 (function main() {
   initView();
@@ -9,12 +10,7 @@ import { initWorkers } from '@Model';
 
   // Run the first time
   runDraw();
-})();
 
-// Register the serviceWorker
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
-  });
-}
+  // Add SW
+  registerSW();
+})();
