f1e8265cdcdcf974096ac05f1c3c9c97b63e60ef
Edison Peñuela <edisonpe961206@hotmail.com>
2021-01-22 22:48:21 -0500

    Add GA event support

--- src/view/Elements/downloadButton.ts
+++ src/view/Elements/downloadButton.ts
@@ -1,3 +1,5 @@
+import { registerDownload } from '../../googleAnalytics';
+
 import { addIcon, e } from '../helpers';
 import { Button } from '../Components/Button';
 import { myCanva } from './canvas';
@@ -6,6 +8,7 @@ import { faFileDownload } from '@fortawesome/free-solid-svg-icons/faFileDownload
 
 export const downloadBtn = Button('Descargar ');
 downloadBtn.addEventListener('click', () => {
+  registerDownload();
   const link = e('a');
   link.download = 'fractal.png';
   link.href = myCanva.toDataURL();
